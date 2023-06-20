b51b83b4f7ae981b260ee2fd7c8effe168571ca0
mariobac1 <mario.moralesbac1@gmail.com>
2023-06-20 15:31:10 -0400

    Se agrega whatsapp en el card, la direccionamiento

--- src/components/molecules/product/Card.jsx
+++ src/components/molecules/product/Card.jsx
@@ -1,12 +1,15 @@
-import { Link } from "react-router-dom"
 import { API_URL, IMAGE_PRODUCT_URL } from "../../../constants/env"
 
-const Card = ({ product }) => {
+const Card = ({ product, whatsapp }) => {
 	const { name, id, price, description } = product
 
 	return (
 		<article className="article-product">
-			<Link to={`/${id}`}>
+			<a
+				href={`${whatsapp}?text=Estoy interesado en ${name}`}
+				target="_blank"
+				rel="noopener noreferrer"
+			>
 				<div className="container-imageProduct">
 					<img
 						className="group-hover/item:ml-1"
@@ -19,7 +22,7 @@ const Card = ({ product }) => {
 					<h3 className="group-hover/item:text-2xl">Precio: Q.{price}</h3>
 					<p className="group-hover/item:font-bold">{description}</p>
 				</div>
-			</Link>
+			</a>
 		</article>
 	)
 }
